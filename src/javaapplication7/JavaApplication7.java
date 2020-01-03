2b4ec670a91a0ce203c1ec578e11c8b8f6037720
karthikeyansa <karthikeyansa39@gmail.com>
2020-01-03 19:41:30 +0530

    added files

---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author KARTHI
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frame1 f1=new frame1();
+        f1.setVisible(true);
+        f1.pack();
+        f1.setLocationRelativeTo(null);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+        
+        
+    }
+    
+}
